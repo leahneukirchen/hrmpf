3a6812269893d1e89eef89e43effe233571e5020
Christian Neukirchen <chneukirchen@gmail.com>
2015-10-11 16:47:31 +0200

    mkhrmpf.sh: more free space.

--- mkhrmpf.sh
+++ mkhrmpf.sh
@@ -5,6 +5,7 @@
 	-C "loglevel=6 printk.time=1 net.ifnames=0" \
 	-r http://repo.voidlinux.eu/current \
 	-r http://repo.voidlinux.eu/current/nonfree \
+	-S 1024 \
 	-B extra/balder10.img \
 	-B extra/mhdd32ver4.6.iso \
 	-B extra/ipxe.iso \
